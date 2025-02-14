d852fa1d106ea11a50dd26a08c81e277bc8230c8
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-02-14 13:22:22 -0500

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,10 @@
+<?php
+ 
+/**
+* @copyright  Bright Cloud Studio
+* @author     Bright Cloud Studio
+* @package    Contao CE Recaptcha
+* @license    LGPL-3.0+
+* @see	       https://github.com/bright-cloud-studio/contao-isotope-ajax-search
+*/
+
