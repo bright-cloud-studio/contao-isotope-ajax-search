fc47e07a62abdadbc878d178d34ddfbf45110f9c
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-02-14 13:16:56 -0500

    Create BcsIsotopeAjaxSearchBundle.php

---
+++ src/BcsIsotopeAjaxSearchBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao Isotope Ajax Search
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-isotope-ajax-search
+ */
+
+namespace Bcs\IsotopeAjaxSearchBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsIsotopeAjaxSearchBundle extends Bundle
+{
+}
